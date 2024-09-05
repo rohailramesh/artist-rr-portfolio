dd1d5d6304ad72b722b1f7269f73f0cbefb1120b
Rohail Ramesh <rohailramesh@hotmail.com>
2024-09-05 12:02:56 +0100

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,55 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
+});
+
+export const metadata: Metadata = {
+  title: "Evently",
+  description: "Evently is a platform for event management.",
+  icons: {
+    icon: "/assets/images/logo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.variable}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+  // return (
+  //   <ClerkProvider>
+  //     <html lang="en">
+  //       <body>
+  //         <header>
+  //           <SignedOut>
+  //             <SignInButton />
+  //           </SignedOut>
+  //           <SignedIn>
+  //             <UserButton />
+  //           </SignedIn>
+  //         </header>
+  //         <main>{children}</main>
+  //       </body>
+  //     </html>
+  //   </ClerkProvider>
+  // );
+}
