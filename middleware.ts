eaf662bd877bfb9ac8f4e0218974786fd5454bc4
Rohail Ramesh <rohailramesh@hotmail.com>
2024-09-05 14:38:08 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,20 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher([
-  "/profile(.*)",
-  "/api/webhook/clerk(.*)",
+const isProtectedRoute = createRouteMatcher(["/profile(.*)"]);
+
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/events/:id",
+  "/api/webhook/clerk",
+  "/api/webhook/stripe",
+  "/api/uploadthing",
 ]);
 
 export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+  // Protect the profile route, and protect all other routes except those that are public
+  if (isProtectedRoute(req) && !isPublicRoute(req)) {
+    auth().protect();
+  }
 });
 
 export const config = {
@@ -17,3 +25,23 @@ export const config = {
     "/(api|trpc)(.*)",
   ],
 };
+
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+// const isProtectedRoute = createRouteMatcher([
+//   "/profile(.*)",
+//   "/api/webhook/clerk(.*)",
+// ]);
+
+// export default clerkMiddleware((auth, req) => {
+//   if (isProtectedRoute(req)) auth().protect();
+// });
+
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+//     // Always run for API routes
+//     "/(api|trpc)(.*)",
+//   ],
+// };
