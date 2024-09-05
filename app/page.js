e3253be3bbda3796b003a372f7a71f262bcb8f1a
Rohail Ramesh <rohailramesh@hotmail.com>
2024-09-05 11:08:54 +0100

    Reset Repo

--- app/page.js
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-import HeroAnimation from "@/components/HeroAnimation";
-export default function Home() {
-  return (
-    <>
-      <HeroAnimation />
-    </>
-  );
-}
--- app/workshop/page.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Workshop() {
-  return <div>Workshop details</div>;
-}
-
-export default Workshop;
