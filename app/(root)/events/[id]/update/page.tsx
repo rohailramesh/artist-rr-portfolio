084041bd93c465d37981e6541d0e47735e0d8837
Rohail Ramesh <rohailramesh@hotmail.com>
2024-09-06 15:13:34 +0100

    Feat: Stripe integrated

--- app/(root)/events/[id]/update/page.tsx
+++ app/(root)/events/[id]/update/page.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import EventForm from "@/components/shared/EventForm";
+import { getEventById } from "@/lib/actions/event.actions";
 import { auth } from "@clerk/nextjs/server";
 
-const UpdateEvent = () => {
+type UpdateEventProps = {
+  params: {
+    id: string;
+  };
+};
+
+const UpdateEvent = async ({ params: { id } }: UpdateEventProps) => {
   const { sessionClaims } = auth();
 
   const userId = sessionClaims?.userId as string;
+  const event = await getEventById(id);
 
   return (
     <>
@@ -16,7 +24,12 @@ const UpdateEvent = () => {
       </section>
 
       <div className="wrapper my-8">
-        <EventForm userId={userId} type="Update" />
+        <EventForm
+          type="Update"
+          event={event}
+          eventId={event._id}
+          userId={userId}
+        />
       </div>
     </>
   );
--- components/shared/CheckoutButton.tsx
+++ components/shared/CheckoutButton.tsx
@@ -5,7 +5,7 @@ import { SignedIn, SignedOut, useUser } from "@clerk/nextjs";
 import Link from "next/link";
 import React from "react";
 import { Button } from "../ui/button";
-// import Checkout from "./Checkout";
+import Checkout from "./Checkout";
 
 const CheckoutButton = ({ event }: { event: IEvent }) => {
   const { user } = useUser();
@@ -26,9 +26,9 @@ const CheckoutButton = ({ event }: { event: IEvent }) => {
             </Button>
           </SignedOut>
 
-          {/* <SignedIn>
+          <SignedIn>
             <Checkout event={event} userId={userId} />
-          </SignedIn> */}
+          </SignedIn>
         </>
       )}
     </div>
