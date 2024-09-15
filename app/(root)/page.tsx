1cd8cccf0eb382a62755ee7d3fced43221533b86
Rohail Ramesh <rohailramesh@hotmail.com>
2024-09-15 19:49:50 +0100

    Update page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -54,7 +54,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
       >
         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
-            <h1 className="h1-bold">Who is ReeBolly?</h1>
+            <h1 className="h1-bold">ReeBolly</h1>
             <p className="p-regular-20 md:p-regular-24">
               Reeha is a trained dancer for 5+ years but has a passion of
               dancing since childhood. Spreading the love of dance and life
