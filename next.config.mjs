3f082d041e3ab420101d73d29cb7f3f51ced1ea6
Rohail Ramesh <rohailramesh@hotmail.com>
2024-09-15 17:08:13 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,10 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, // replace "*" with your actual origin, e.g., "https://www.reebolly.co.uk"
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://www.reebolly.co.uk",
+          }, // replace "*" with your actual origin, e.g., "https://www.reebolly.co.uk"
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,DELETE,PATCH,POST,PUT",
