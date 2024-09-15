e9834a27fe1ad079d56e742460ba38a6bb3d1723
Rohail Ramesh <rohailramesh@hotmail.com>
2024-09-15 15:01:04 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Adding headers for CORS configuration
+  async headers() {
+    return [
+      {
+        // Matching all API routes for CORS
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace "*" with your actual origin, e.g., "https://www.reebolly.co.uk"
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
+
+  // Existing image configuration
   images: {
-    remotePatterns: ["utfs.io"],
     remotePatterns: [
       {
         protocol: "https",
@@ -12,4 +35,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
